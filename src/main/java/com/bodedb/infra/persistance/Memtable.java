5be8c9ecb3865209b9a15eadcc0e3d86b168223d
David Ramos <drs@a.recife.ifpe.edu.br>
2026-02-24 22:22:43 -0300

    fix: corrige problema de thread safety e adiciona estrategisa de epoch based reclamation EBR

--- src/main/java/com/bodedb/infra/persistance/Memtable.java
+++ src/main/java/com/bodedb/infra/persistance/Memtable.java
@@ -12,16 +12,21 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
+class ThreadedTable<K, V> {
+    public final ConcurrentSkipListMap<K, V> map = new ConcurrentSkipListMap<>();
+    public AtomicInteger size = new AtomicInteger(0);
+    public AtomicInteger holders = new AtomicInteger(0);
+}
+
 public class Memtable<K extends Comparable<K>, V> {
 
-    private AtomicReference<ConcurrentSkipListMap<K, V>> table = new AtomicReference<>(new ConcurrentSkipListMap<>());
-    private final Integer MEMTABLE_SIZE_LIMIT = 200;
-    private final WAL<K,V> WALService;
+    private AtomicReference<ThreadedTable<K, V>> table = new AtomicReference<>(new ThreadedTable<K, V>());
+    private final Integer MEMTABLE_SIZE_LIMIT = 4;
+    private final WAL<K, V> WALService;
 
     Serializer<K> keySerializer;
     Serializer<V> valueSerializer;
     AtomicBoolean rotating = new AtomicBoolean(false);
-    AtomicInteger sz = new AtomicInteger(0);
 
     boolean isSynchronousCommitActive = Boolean.parseBoolean(System.getenv("BODEDB_SYNCHRONOUS_WAL_COMMIT_ACTIVE"));
 
@@ -29,7 +34,8 @@ public class Memtable<K extends Comparable<K>, V> {
         this.keySerializer = keySerializer;
         this.valueSerializer = valueSerializer;
 
-        System.out.println("[MEMTABLE - WAL persistance type] synchronous commit is " + (this.isSynchronousCommitActive ? "active" : "deactivated"));
+        System.out.println("[MEMTABLE - WAL persistance type] synchronous commit is "
+                + (this.isSynchronousCommitActive ? "active" : "deactivated"));
 
         try {
             this.WALService = new WAL<>();
@@ -44,41 +50,67 @@ public class Memtable<K extends Comparable<K>, V> {
 
         CompletableFuture<Boolean> resultWalLine = WALService.write(key, value);
 
-        if(isSynchronousCommitActive) resultWalLine.get();
+        if (isSynchronousCommitActive)
+            resultWalLine.get();
 
-        SortedMap<K,V> currentTable = table.get();
-        V result = currentTable.put(key, value);
+        ThreadedTable<K, V> state;
 
-        if(result != null) return  result;
+        while (true) {
+            state = this.table.get();
 
-        //it's not completely guaranteed that flushed sstable will contain exactly MEMTABLE_SIZE_LIMIT entries.
-        //Because incrementAndGet and check of rotating flag can happen concurrently. There`s a race condition.
-        //It means that flushed file can contain more entries than the limit defined even if the thread that has inserted
-        //into memtable wasn`t the one  responsible for calling the rotating function.
-        if(sz.incrementAndGet() >= MEMTABLE_SIZE_LIMIT && rotating.compareAndSet(false, true)) {
-            System.out.println("[MEMTABLE] Limit reached! Flushing data");
-            try{
-                rotate();
-            }finally {
-                sz.set(0);
-            }
+            state.holders.incrementAndGet();
+
+            if (state == this.table.get())
+                break;
+
+            state.holders.decrementAndGet();
         }
 
-        return result;
+        try {
+            V result = state.map.put(key, value);
+
+            // it's not completely guaranteed that flushed sstable will contain exactly
+            // MEMTABLE_SIZE_LIMIT entries.
+            // Because incrementAndGet and check of rotating flag can happen concurrently.
+            // There`s a race condition.
+            // It means that flushed file can contain more entries than the limit defined
+            // even if the thread that has inserted
+            // into memtable wasn`t the one responsible for calling the rotating function.
+            if (result == null && state.size.incrementAndGet() >= MEMTABLE_SIZE_LIMIT) {
+                if (rotating.compareAndSet(false, true)) {
+                    System.out.println("[MEMTABLE] Limit reached! Flushing data");
+                    rotate();
+                }
+            }
+
+            return result;
+
+        } finally {
+            state.holders.decrementAndGet();
+        }
+
     }
 
     public void rotate() {
-        SortedMap<K,V> memtableToFlush = table.getAndSet(new ConcurrentSkipListMap<>());
-        flush(Collections.unmodifiableSortedMap(memtableToFlush));
+        ThreadedTable<K, V> memtableToFlush = table.getAndSet(new ThreadedTable<K, V>());
+
+        while (memtableToFlush.holders.get() > 0) {
+            Thread.onSpinWait();
+        }
+
+        flush(Collections.unmodifiableSortedMap(memtableToFlush.map));
     }
 
-    private void flush(SortedMap<K,V> table) {
+    private void flush(SortedMap<K, V> table) {
 
         Runnable writeToDisk = () -> {
-            new SStable<>(table, keySerializer, valueSerializer).writeToFile();
-            rotating.set(false);
+            try {
+                new SStable<>(table, keySerializer, valueSerializer).writeToFile();
+            } finally {
+                rotating.set(false);
+            }
         };
 
-       Thread.ofVirtual().start(writeToDisk);
+        Thread.ofVirtual().start(writeToDisk);
     }
 }
